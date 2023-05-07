6881fff72219cbaf4754ce5875bce8952d8780b1
Bryce Eppler <eppler97@gmail.com>
2023-05-06 17:16:50 -0700

    init

---
+++ index.ts
@@ -0,0 +1,65 @@
+import express, { Express, Request, Response } from 'express';
+import session from 'express-session';
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(session({
+  secret: 'your-session-secret',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user: Express.User, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user: Express.User, done) => {
+  done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    callbackURL: 'http://localhost:' + port + '/auth/google/callback'
+  },
+  (accessToken, refreshToken, profile, done) => {
+    done(null, profile);
+  }
+));
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req: Request, res: Response) => {
+    res.redirect('/profile');
+  }
+);
+
+app.get('/profile', (req: Request, res: Response) => {
+  if (req.user) {
+    res.send(`<h1>Hello, ${(req.user as any).displayName}!</h1><a href="/logout">Logout</a>`);
+  } else {
+    res.redirect('/auth/google');
+  }
+});
+
+app.get('/logout', (req: Request, res: Response) => {
+  req.logout(
+    () => console.log('User logged out')
+  );
+  res.redirect('/profile');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
