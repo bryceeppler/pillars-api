ed89fc280615a909d0aafc88f6aca3dfad83b5f5
Bryce Eppler <eppler97@gmail.com>
2023-05-06 17:25:59 -0700

    updating inital template

--- index.ts
+++ index.ts
@@ -1,19 +1,21 @@
-import express, { Express, Request, Response } from 'express';
-import session from 'express-session';
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import session from "express-session";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use(session({
-  secret: 'your-session-secret',
-  resave: false,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: "your-session-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -26,38 +28,47 @@ passport.deserializeUser((user: Express.User, done) => {
   done(null, user);
 });
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID!,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: 'http://localhost:' + port + '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    done(null, profile);
-  }
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: "http://localhost:" + port + "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      done(null, profile);
+    }
+  )
+);
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
   (req: Request, res: Response) => {
-    res.redirect('/profile');
+    res.redirect("/profile");
   }
 );
 
-app.get('/profile', (req: Request, res: Response) => {
+app.get("/profile", (req: Request, res: Response) => {
   if (req.user) {
-    res.send(`<h1>Hello, ${(req.user as any).displayName}!</h1><a href="/logout">Logout</a>`);
+    res.send(
+      `<h1>Hello, ${
+        (req.user as any).displayName
+      }!</h1><a href="/logout">Logout</a>`
+    );
   } else {
-    res.redirect('/auth/google');
+    res.redirect("/auth/google");
   }
 });
 
-app.get('/logout', (req: Request, res: Response) => {
-  req.logout(
-    () => console.log('User logged out')
-  );
-  res.redirect('/profile');
+app.get("/logout", (req: Request, res: Response) => {
+  req.logout(() => console.log("User logged out"));
+  res.redirect("/profile");
 });
 
 app.listen(port, () => {
